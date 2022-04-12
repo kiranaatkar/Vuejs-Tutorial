e29ffe427738ebe4b788f1b9719b179cd83ae5a2
Kiran Aatkar <kiranaatkar@gmail.com>
2022-04-12 12:28:05 +0100

    Completed CDN project

---
+++ userGenerator/README.md
@@ -0,0 +1,3 @@
+Project uses the Vue CDN (content delivery network) where you directly mount to app.
+
+could be used for very small projects without multiple components
--- userGenerator/app.js
+++ userGenerator/app.js
@@ -1,3 +1,25 @@
-const app = Vue.createApp({ template: '<h1>Hello World</h1>' });
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+    };
+  },
+  methods: {
+    async getUser() {
+      const response = await fetch('https://randomuser.me/api');
+      const { results } = await response.json();
+      const { name, gender, email, picture } = results[0];
+      this.firstName = name.first;
+      this.lastName = name.last;
+      this.email = email;
+      this.gender = gender;
+      this.picture = picture.large;
+    },
+  },
+});
 
 app.mount('#app'); // mounts to div on index.html with id app
