82dcc7b56588e97a0d3f461aaebde516519e2e35
Kiran Aatkar <kiranaatkar@gmail.com>
2022-04-13 10:18:39 +0100

    UI finished

---
+++ taskTracker/task-tracker/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <myButton
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import myButton from './Button';
+
+export default {
+  name: 'myHeader',
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    myButton,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ taskTracker/task-tracker/src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div :key="task.id" v-for="task in tasks">
+      <myTask
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import myTask from './Task.vue';
+export default {
+  name: 'myTasks',
+  props: {
+    tasks: Array,
+  },
+  components: {
+    myTask,
+  },
+  emite: ['delete-task', 'toggle-reminder'],
+};
+</script>
